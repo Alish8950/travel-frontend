33820aaa7f151c39f3829252fd3adcd51c3e068e
Alish <alishallawadhi5@gmail.com>
2025-01-31 16:54:56 +0530

    Add logo asset and update login/signup pages with new styles and structure

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import Logo from "./images/travel-logo.svg";
+
+export { Logo };
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import React from "react";
+import { Logo } from "../assets";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+          <div className="mx-auto w-40">
+            <img src={Logo} alt="Travel Logo" className="w-full" />
+          </div>
+          <h2 className="text-2xl font-bold text-center text-gray-900">
+            Signup
+          </h2>
+          <form className="space-y-6">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username*
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-light focus:border-primary-light sm:text-sm"
+              />
+            </div>
+            <div className="md:flex md:justify-between ">
+              <div>
+                <label
+                  htmlFor="firstname"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  First Name*
+                </label>
+                <input
+                  id="firstname"
+                  name="firstname"
+                  type="text"
+                  autoComplete="firstname"
+                  required
+                  className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-light focus:border-primary-light sm:text-sm"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="lastname"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Last Name*
+                </label>
+                <input
+                  id="lastname"
+                  name="lastname"
+                  type="text"
+                  autoComplete="lastname"
+                  required
+                  className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-light focus:border-primary-light sm:text-sm"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address*
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-light focus:border-primary-light sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password*
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-light focus:border-primary-light sm:text-sm"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+               Confirm Password*
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-light focus:border-primary-light sm:text-sm"
+              />
+            </div>
 
-export default Signup
+            <div className="text-center">
+              <button
+                type="submit"
+                className="w-full px-4 py-2 text-sm font-medium text-white bg-primary border border-transparent rounded-md shadow-sm hover:bg-primary-light-hover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-light"
+              >
+                Sign in
+              </button>
+              <div className="mt-4">
+                <span className="font-medium text-gray-700">
+                  Already have an account?{" "}
+                </span>
+                <Link
+                  className="font-medium text-primary hover:text-primary-light"
+                  to="/"
+                >
+                  Login
+                </Link>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
